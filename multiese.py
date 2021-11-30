3257dfc362689c21684ac28024443e5fef832dc2
Yuka Akinobu <sx2_vn_yuka@outlook.jp>
2021-11-30 21:05:42 +0900

    added comment

--- multiese.py
+++ multiese.py
@@ -79,11 +79,11 @@ if __name__ == '__main__':
     parser.add_argument('--pyfirst', action='store_true')
     parser.add_argument('-o', '--out', default=None)
     parser.add_argument('--max', type=int, default=3)
-    parser.add_argument('--type-none', action='store_true')
-    parser.add_argument('--type-prefix', action='store_true')
-    parser.add_argument('--type-suffix', action='store_true')
-    parser.add_argument('--partial', action='store_true')
-    parser.add_argument('--change-ga', action='store_true')
+    parser.add_argument('--type-none', action='store_true')    # 型情報をつけない
+    parser.add_argument('--type-prefix', action='store_true')  # 型情報を先頭につける (e.g.: データフレームdf)
+    parser.add_argument('--type-suffix', action='store_true')  # 型情報を後ろにつける (e.g.: dfデータフレーム)
+    parser.add_argument('--partial', action='store_true')      # partial
+    parser.add_argument('--change-ga', action='store_true')    # 助詞の「が」と「は」をランダムに入れ替える
     #parser.add_argument('--files', nargs='*')
     args = parser.parse_args()
     option = vars(args)   # vars(args) => dict
