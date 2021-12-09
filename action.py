8b4c38ec6daa5cf34e595579ec029366806cd8f1
kkuramitsu <kkuramitsu@gmail.com>
2021-12-10 01:10:38 +0900

    refactor alt in action.py

--- action.py
+++ action.py
@@ -1,9 +1,5 @@
 import random
-from choice import alt, random_seed, optional_choice
-
-
-def fin(s):
-    return s.replace('/', '')
+from choice import alt
 
 
 def remove_whether(sentence):
@@ -61,7 +57,7 @@ def perform_andor(pairs, option):
             code_or = f'{code} or'
             sentence, _ = remove_whether(sentence)
             sentence_and = sentence + '、かつ、'
-            sentence_or = sentence + optional_choice(option, '、または、|、もしくは、')
+            sentence_or = sentence + alt('、または、|、もしくは、')
             pairs_andor.append((sentence_and, code_and))
             pairs_andor.append((sentence_or, code_or))
     return pairs_andor
@@ -73,11 +69,11 @@ def perform_if(pairs, option):
     for sentence, code in pairs:
         code_if = f'if {code} :'
         sentence, _ = remove_whether(sentence)
-        sentence_if = optional_choice(option, 'もし|もし|') + sentence
+        sentence_if = alt('もし|もし|') + sentence
         if sentence.endswith('い'):   # 形容詞のとき
-            sentence_if += optional_choice(option, 'ならば|なら|場合|とき')
+            sentence_if += alt('ならば|なら|場合|とき')
         else:
-            sentence_if += optional_choice(option, 'ならば|なら|の場合|のとき')
+            sentence_if += alt('ならば|なら|の場合|のとき')
         pairs_if.append((sentence_if, code_if))
     return pairs_if
 
@@ -101,67 +97,66 @@ def isGroup2Verb(s):
     return False
 
 
-def transform_verb_and_then(s, option, factor=-1):
+def transform_verb_and_then(s):
     if s.endswith('する'):
-        return s[:-2]+alt('して、|し、', option, factor)
+        return s[:-2]+alt('して、|し、')
     if s.endswith('ずる'):  # 論ずる　
-        return s[:-2]+alt('じて、|じ、', option, factor)
+        return s[:-2]+alt('じて、|じ、')
     if isGroup2Verb(s):  # グループ２ # ラ行と区別が難しい
-        return s[:-1]+alt('、|、|て、', option, factor)
+        return s[:-1]+alt('、|、|て、')
     if s.endswith('く'):  # 書く
-        return s[:-1]+alt('き、|いて、', option, factor)
+        return s[:-1]+alt('き、|いて、')
     if s.endswith('す'):  # 探す
-        return s[:-1]+alt('し、|して、', option, factor)
+        return s[:-1]+alt('し、|して、')
     if s.endswith('つ'):  # 勝つ
-        return s[:-1]+alt('ち、|って、', option, factor)
+        return s[:-1]+alt('ち、|って、')
     if s.endswith('ぬ'):  # 死ぬ
-        return s[:-1]+alt('に、|んて、', option, factor)
+        return s[:-1]+alt('に、|んて、')
     if s.endswith('む'):  # 読む
-        return s[:-1]+alt('み、|んで、', option, factor)
+        return s[:-1]+alt('み、|んで、')
     if s.endswith('る'):  # 切る   ラ行
-        return s[:-1]+alt('り、|って、', option, factor)
+        return s[:-1]+alt('り、|って、')
     if s.endswith('う'):  # 笑う
-        return s[:-1]+alt('い、|って、', option, factor)
+        return s[:-1]+alt('い、|って、')
     if s.endswith('ぐ'):  # 防ぐ
-        return s[:-1]+alt('ぎ、|いで、', option, factor)
+        return s[:-1]+alt('ぎ、|いで、')
     if s.endswith('ぶ'):  # 遊ぶ
-        return s[:-1]+alt('び、|んで、', option, factor)
+        return s[:-1]+alt('び、|んで、')
     return None
 
 
-def transform_verb_and_noun(s, option, factor=-1):
+def transform_verb_and_noun(s):
     if s.endswith('する'):
-        return s[:-2]+alt('した|した|された', option, factor)
+        return s[:-2]+alt('した|した|された')
     if s.endswith('ずる'):  # 論ずる　
-        return s[:-2]+alt('じた|じた|じられた、', option, factor)
+        return s[:-2]+alt('じた|じた|じられた、')
     if isGroup2Verb(s):  # グループ２ # ラ行と区別が難しい
-        return s[:-1]+alt('た|た|られた', option, factor)
+        return s[:-1]+alt('た|た|られた')
     if s.endswith('く'):  # 書く
-        return s[:-1]+alt('いた|いた|かれた', option, factor)
+        return s[:-1]+alt('いた|いた|かれた')
     if s.endswith('す'):  # 探す
-        return s[:-1]+alt('した|した|された', option, factor)
+        return s[:-1]+alt('した|した|された')
     if s.endswith('つ'):  # 勝つ
-        return s[:-1]+alt('った|った|たれた', option, factor)
+        return s[:-1]+alt('った|った|たれた')
     if s.endswith('ぬ'):  # 死ぬ
-        return s[:-1]+alt('んだ|んだ|なれた', option, factor)
+        return s[:-1]+alt('んだ|んだ|なれた')
     if s.endswith('む'):  # 読む
-        return s[:-1]+alt('んだ|んだ|まれた', option, factor)
+        return s[:-1]+alt('んだ|んだ|まれた')
     if s.endswith('る'):  # 切る   ラ行
-        return s[:-1]+alt('った|った|られた', option, factor)
+        return s[:-1]+alt('った|った|られた')
     if s.endswith('う'):  # 笑う
-        return s[:-1]+alt('った|った|われた、', option, factor)
+        return s[:-1]+alt('った|った|われた、')
     if s.endswith('ぐ'):  # 防ぐ
-        return s[:-1]+alt('いだ|いだ|がれた', option, factor)
+        return s[:-1]+alt('いだ|いだ|がれた')
     if s.endswith('ぶ'):  # 遊ぶ
-        return s[:-1]+alt('んだ|んだ|ばれた', option, factor)
+        return s[:-1]+alt('んだ|んだ|ばれた')
     return None
 
 
 def perform_noun(pairs, option):  # 名詞に変える
     pairs_noun = []
     for sentence, code in pairs:
-        sentence_and_noun = transform_verb_and_noun(
-            sentence, '', option, factor=2)
+        sentence_and_noun = transform_verb_and_noun(sentence)
         if sentence_and_noun:
             noun = option.get('action-with', '結果')
             noun = alt(noun + '|結果', option, factor=3)
@@ -173,21 +168,18 @@ def perform_let(pairs, option):  # 代入文に変える
     pairs_let = []
     for sentence, code in pairs:
         code_let = f'X = {code}'
-        sentence_and_then = transform_verb_and_then(sentence, option, factor=6)
+        sentence_and_then = transform_verb_and_then(sentence)
         if sentence_and_then:
-            sentence_let = sentence_and_then + 'X' + \
-                optional_choice(option, 'にする|とする|に代入する')
+            sentence_let = sentence_and_then + 'X' + alt('にする|とする|に代入する')
             pairs_let.append((sentence_let, code_let))
-            noun = optional_choice(option, option.get('action-with', '結果'))
+            noun = alt(option.get('action-with', '結果'))
             sentence = transform_verb_and_noun(sentence, option, 3) + noun
-        sentence_let = sentence + 'をX' + \
-            optional_choice(option, 'にする|とする|に代入する')
+        sentence_let = sentence + 'をX' + alt('にする|とする|に代入する')
         pairs_let.append((sentence_let, code_let))
         if sentence.endswith('かどうか'):
             # 小さいかどうか　=> 小さいとき、Xを真とする
             s, _ = remove_whether(sentence)
-            sentence_let = s + 'とき、Xを真' + \
-                optional_choice(option, 'にする|とする', seed=1)
+            sentence_let = s + 'とき、Xを真' + alt('にする|とする')
             pairs_let.append((sentence_let, code_let))
     return pairs_let
 
@@ -197,20 +189,16 @@ def perform_let_self(pairs, option):
     for sentence, code in pairs:
         name = code[:code.find('.')]
         code_let_self = f'{name} = {code}'
-        sentence_and_then = transform_verb_and_then(
-            sentence, random_seed(option))
+        sentence_and_then = transform_verb_and_then(sentence)
         if sentence_and_then:
-            sentence_let_self = sentence_and_then + name + \
-                optional_choice(option, 'にする|とする|に代入する')
+            sentence_let_self = sentence_and_then + name + alt('にする|とする|に代入する')
             pairs_let_self.append((sentence_let_self, code_let_self))
-            sentence_let_self = sentence_and_then + \
-                optional_choice(option, '置き換える|再代入する')
+            sentence_let_self = sentence_and_then + alt('置き換える|再代入する')
             pairs_let_self.append((sentence_let_self, code_let_self))
             sentence = transform_verb_and_noun(sentence, '結果', 0)
-        sentence_let_self = sentence + 'を' + name + \
-            optional_choice(option, 'にする|とする|に代入する')
+        sentence_let_self = sentence + 'を' + name + alt('にする|とする|に代入する')
         pairs_let_self.append((sentence_let_self, code_let_self))
-        sentence_let_self = sentence + optional_choice(option, 'を置き換える|を再代入する')
+        sentence_let_self = sentence + alt('を置き換える|を再代入する')
         pairs_let_self.append((sentence_let_self, code_let_self))
     return pairs_let_self
 
@@ -224,15 +212,12 @@ def perform_inplace(pairs, option):
         else:
             # e.g.: df.drop('price', axis=1)
             code_inplace = code[:-1] + ', inplace=True)'
-        sentence_and_then = transform_verb_and_then(
-            sentence, random_seed(option))
+        sentence_and_then = transform_verb_and_then(sentence)
         if sentence_and_then:
-            sentence_inplace = sentence_and_then + \
-                optional_choice(option, 'インプレースする|置き換える|書き換える')
+            sentence_inplace = sentence_and_then + alt('インプレースする|置き換える|書き換える')
             pairs_inplace.append((sentence_inplace, code_inplace))
             sentence = transform_verb_and_noun(sentence, '結果', 0)
-        sentence_inplace = sentence + \
-            optional_choice(option, 'でインプレースする|で置き換える|で書き換える')
+        sentence_inplace = sentence + alt('でインプレースする|で置き換える|で書き換える')
         pairs_inplace.append((sentence_inplace, code_inplace))
     return pairs_inplace
 
@@ -241,7 +226,7 @@ def perform_then(pairs, option):
     pairs_then = []
     if option.get('partial', True):
         for sentence, code in pairs:
-            verb = transform_verb_and_then(sentence, option)
+            verb = transform_verb_and_then(sentence)
             if verb:
                 pairs_then.append((verb, code+'.'))
     return pairs_then
@@ -276,7 +261,7 @@ def perform_it(pairs, option):
 def perform_check(pairs, option):
     pairs_check = []
     for sentence, code in pairs:
-        verb = transform_verb_and_then(sentence, option)
+        verb = transform_verb_and_then(sentence)
         if not verb:
             sentence_check = sentence + alt('を表示する|を確認する|を調べる|を見る')
             pairs_check.append((sentence_check, code))
@@ -286,7 +271,7 @@ def perform_check(pairs, option):
 def perform_get(pairs, option):
     pairs_get = []
     for sentence, code in pairs:
-        verb = transform_verb_and_then(sentence, option)
+        verb = transform_verb_and_then(sentence)
         if not verb:
             sentence_get = sentence + alt('を取得する|を得る|をえる|を抽出する')
             pairs_get.append((sentence_get, code))
@@ -296,7 +281,7 @@ def perform_get(pairs, option):
 def perform_calc(pairs, option):
     pairs_calc = []
     for sentence, code in pairs:
-        verb = transform_verb_and_then(sentence, option)
+        verb = transform_verb_and_then(sentence)
         if not verb:
             sentence_calc = sentence + alt('を計算する|を求める|を算出する')
             pairs_calc.append((sentence_calc, code))
