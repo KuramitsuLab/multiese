8638a074f9d078af98055f877aafc3d6833ea70f
Kimio Kuramitsu <kkuramitsu@gmail.com>
2022-05-18 15:03:50 +0900

    new codegen2

---
+++ test_codegen2.py
@@ -0,0 +1,313 @@
+import keyword
+from io import BytesIO
+from operator import is_
+import token
+from tokenize import tokenize, open
+from importlib import import_module
+import sys
+import random
+import csv
+import copy
+import collections
+import datetime
+import builtins
+import pandas as pd
+import numpy as np
+import warnings
+
+warnings.simplefilter('ignore')
+
+df = pd.DataFrame(data={'A': [1, 2, 3],
+                        'B': [1, 1, 0],
+                        'C': [1, 0, 1]})
+df2 = pd.DataFrame(data={'A': [1, 2, 3],
+                         'B': ["a", "b", "c"],
+                         'C': ["111", "01", "1"]})
+
+
+class P(object):
+    def __init__(self):
+        self.A = 1
+        self.B = 0
+
+
+from io import BytesIO
+from tokenize import tokenize, open
+import re
+
+pattern = re.compile(r'[\(, .\+\-\)]')
+
+def tokenize_pycode(code):
+    try:
+        ss=[]
+        tokens = tokenize(BytesIO(code.encode('utf-8')).readline)
+        for toknum, tokval, _, _, _ in tokens:
+            if toknum != 62 and tokval != '' and tokval != 'utf-8':
+                ss.append(tokval)
+        return ss
+    except:
+        return pattern.split(code)
+
+
+class Missing:
+
+    def __init__(self, name=''):
+        self.msg = name
+
+    def __str__(self):
+        return self.msg
+
+    def __repr__(self):
+        return self.msg
+
+    def __getattr__(self, name):
+        return Missing(f'{self.msg}.{name}')
+
+    def __call__(self, *args, **kwargs):
+        if len(kwargs) == 0:
+            return Missing(f'{self.msg}{args}')
+        else:
+            return Missing(f'{self.msg}{args}{dict(**kwargs)}')
+
+    # def __getitem__(self, index):
+    #     return self.msg[index]
+
+
+MUTABLES = {type([]), type({}), type(set()), type(bytearray()), type(pd.DataFrame())}
+
+def copy_mutable(o):
+    t = type(o)
+    if t in MUTABLES:
+        return copy.copy(o)
+    return o
+
+def is_assignment(line):
+    tokens = tokenize_pycode(line)
+    for token in tokens:
+        if token == '=':
+            return True
+        if token == '(' or token== '[':
+            break
+    return False
+
+def is_expression(line):
+    tokens = tokenize_pycode(line)
+    if len(tokens) > 0 and keyword.iskeyword(tokens[0]):
+        return False
+    for token in tokens:
+        if token == '=':
+            return False
+        if token == '(' or token == '[':
+            break
+    return True
+
+def shorten_name(name):
+    return name[:-1] if name[-1].isdigit() else name
+
+def extract_vars(globals, locals):
+    for name, value in locals.items():
+        #name = shorten_name(name)
+        if name not in dir(builtins):
+            values = globals.get(name, [])
+            values.append(value)
+            globals[name] = values
+
+def read_vars(filename, evars):
+    globals = {'print': Missing('print')}
+    with open(filename) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith('import ') or line.startswith('from '):
+                try:
+                    exec(line, None, globals)
+                except:
+                    print('Error', line)
+            if is_assignment(line):
+                locals = {}
+                vars = collections.ChainMap(locals, globals)
+                try:
+                    exec(line, None, vars)
+                    extract_vars(evars, locals)
+                    for name, value in locals.items():
+                        globals[name] = value
+                except Exception as e:
+                    print('Error2', line, e)
+
+
+VALUE = set(['True', 'False', 'None'])
+
+def iskeyword(s):
+    if s in VALUE or s in dir(builtins):
+        return True
+    return keyword.iskeyword(s)
+
+
+def extract_names(code):
+    names = []
+    try:
+        tokens = tokenize(BytesIO(code.encode('utf-8')).readline)
+        prev = ''
+        for toknum, tokval, _, _, _ in tokens:
+            # print(toknum, tokval)
+            if toknum == token.NAME and not iskeyword(tokval) and prev != '.':
+                names.append(tokval)
+            if tokval == '=' and prev in vars:
+                names.pop()
+            prev = tokval
+    finally:
+        return names
+
+
+def match(value1, value2):
+    s1 = str(value1)
+    s2 = str(value2)
+    if 'at 0x' in s1 and 'at 0x' in s2:
+        s1, _, _ = s1.partition('at 0x')
+        s2, _, _ = s2.partition('at 0x')
+    return s1 == s2
+
+def compare_vars(vars, vars2):
+    if len(vars) != len(vars2):
+        return False
+    if '_' in vars and '_' in vars2:
+        return match(vars['_'], vars['_'])
+    for key in vars:
+        if key not in vars2:
+            return False
+        if not match(vars[key], vars2[key]):
+            return False
+    return True
+
+def iskeyword(s):
+    if s in VALUE or s in dir(builtins):
+        return True
+    return keyword.iskeyword(s)
+
+def modify_code(code):
+    if is_expression(code):
+        return '_ = ' + code
+    return code
+    
+
+
+class TestSuite:
+    def __init__(self, name_values):
+        self.name_values = name_values
+        self.tested = 0
+        self.untested = 0
+        self.syntax_errors = 0
+        self.tested_ok = 0
+        self.missing = 0
+
+    def test_code(self, code, code2, check_exact_match=True):
+        self.tested += 1
+        code = modify_code(code)
+        code2 = modify_code(code2)
+        self.epoch_succ = 0
+        for _ in range(4):
+            if self.try_test_code(code, code2) == False:
+                return 
+        if self.epoch_succ == 4:
+            self.tested_ok += 1
+        # if self.epoch_succ == 0:
+
+    def choose_vars(self, code, vars):
+        names = extract_names(code)
+        for name in names:
+            if name in self.name_values:
+                value = random.sample(self.name_values[name], k=1)
+                vars[name] = copy_mutable(value)
+            else:
+                vars[name] = Missing(name)
+        vars2 = {}
+        for name, value in vars.items():
+            vars2[name] = copy_mutable(value)
+        return vars, vars2
+
+    def try_test_code(self, code, code2):
+        globals = {
+            'print': Missing('print'),
+            'input': Missing('input'),
+            'list': lambda x: [x],
+            'open': Missing('open'),
+        }
+        locals = {}
+        globals, globals2 = self.choose_vars(code, globals)
+        vars = collections.ChainMap(locals, globals)
+        try:
+            exec(code, None, vars)
+        except SyntaxError:
+            self.untested += 1
+            return False
+        except Exception as e:
+            locals['_'] = e
+        locals2 = {}
+        vars2 = collections.ChainMap(locals2, globals2)
+        try:
+            exec(code2, None, vars2)
+            print(locals2)
+        except SyntaxError:
+            self.syntax_errors += 1
+            return False
+        except Exception as e:
+            locals2['_'] = e
+            print('FAILED', str(e), code2)
+        if compare_vars(locals, locals2) == True:
+            self.epoch_succ += 1
+        return True
+
+
+
+def read_tsv(filename, index=1, pred_index=2):
+    ss = []
+    try:
+        with open(filename) as f:
+            reader = csv.reader(f, delimiter="\t")
+            for row in reader:
+                ss.append((row[index], row[pred_index]))
+    except:
+        with open(filename) as f:
+            reader = csv.reader(f, delimiter="\t")
+            for row in reader:
+                ss.append((row[1], row[1]))
+    return ss
+
+#main()
+
+def main():
+    vars = {}
+    tsvfile = None
+    for file in sys.argv[1:]:
+        if file.endswith('.py'):
+            read_vars(file, vars)
+        if file.endswith('.tsv'):
+            tsvfile = file
+    suite = TestSuite(vars)
+    if tsvfile is not None:
+        ss = read_tsv(tsvfile)
+        for code, code2 in ss:
+            suite.test_code(code, code2)
+    else:
+        suite.test_code('n + n1', 'n + n1')
+    
+    print(f'Test Count {suite.tested}')
+    print(f' Syntax Error {suite.syntax_errors} {suite.syntax_errors/suite.tested:.5f}')
+    print(f' Untested {suite.untested} {suite.untested/suite.tested:.5f}')
+    print(f' Pass {suite.tested_ok} {suite.tested_ok/suite.tested:.5f} ')
+    print(
+        f' Missing {suite.missing} {suite.missing/suite.tested:.5f}') 
+
+
+main()
+
+
+
+def test():
+    globals = {'print': Missing('print')}
+    locals = {}
+    vars = collections.ChainMap(locals, globals)
+    exec('import pandas as pd', None, vars)
+    exec('a=1', None, vars)
+    exec('b=1', None, vars)
+    exec('_ = c = print(a+b)', None, vars)
+    print(locals)
